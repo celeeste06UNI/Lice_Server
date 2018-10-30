1e4bd004b8023bcfe28b19296e7217966a92651d
celeste <celeste@192.168.1.73>
2018-10-30 20:26:22 +0100

    intentando subir un xml

---
+++ spring-security-server/src/main/java/com/mkyong/datamodelReader/mysqlReader.java
@@ -0,0 +1,15 @@
+package com.mkyong.datamodelReader;
+
+import java.util.List;
+import org.jdom.Document;
+import com.mkyong.model.DataModel;
+
+public class mysqlReader implements xmlReader {
+
+	public List<DataModel> reader(Document doc) {
+		List<DataModel> rows = null;
+		return rows ;
+		
+	}
+
+}
